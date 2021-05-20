15429119a594a21113c2bc8697b0a2006be84fca
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-20 17:46:10 +0200

    Initial Commit

---
+++ SwipeUI/SwipeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeUIApp.swift
+//  SwipeUI
+//
+//  Created by Jorge Giannotta on 20/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwipeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
